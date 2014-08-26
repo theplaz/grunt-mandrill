a9bf72c452abbaf5399485c311aebf19947b2ad2
Michael Plasmeier <theplaz@yahoo-inc.com>
2014-08-25 22:41:58 -0700

    fix it so it works

--- tasks/mandrill.js
+++ tasks/mandrill.js
@@ -62,28 +62,45 @@ module.exports = function(grunt){
   grunt.registerMultiTask('mandrilltemplate','Add email template to mandrill', function(){
     var done = this.async();
     var options = _.pick(this.data.options,['template_name', 'from_email', 'from_name', 'subject', 'code', 'text', 'publish', 'labels']);
+    // Setup node-mandrill with the api
     mandrill_client = new mandrill.Mandrill(this.data.options.key);
     var to = [];
 
-    console.log(this.filesSrc);
     if(this.filesSrc.length > 0){
       _.each(this.filesSrc,function(filepath){
         if(!options.code){
-          options.code = grunt.file.read(filepath);
+          code = grunt.file.read(filepath);
+        } else {
+          code = options.code;
         }
         if(!options.template_name){
           basename = path.basename(filepath);
           extension = path.extname(filepath);
-          name = basename.substr(0, basename.length - extension.length)
-          console.log(name);
+          template_name = basename.substr(0, basename.length - extension.length);
+          //console.log(template_name);
+        } else {
+          template_name = options.template_name;
         }
-        mandrill_client.templates.add({"name": options.template_name, "from_email": options.from_email, "from_name": options.from_name, "subject": options.subject, "code": options.code, "text": options.text, "publish": options.publish, "labels": options.labels}, function(result) {
+        mandrill_client.templates.add({"name": template_name, "from_email": options.from_email, "from_name": options.from_name, "subject": options.subject, "code": code, "text": options.text, "publish": options.publish, "labels": options.labels}, function(result) {
             grunt.log.writeln(result);
             console.log(result);
         }, function(e) {
-          // Mandrill returns the error as an object with name and message keys
-            grunt.log.writeln('A mandrill error occurred: ' + e.name + ' - ' + e.message);
-            console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+            // Mandrill returns the error as an object with name and message keys
+            if (e.message.indexOf("already exists")) {
+               //expected
+            } else {
+               grunt.log.writeln('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+               console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+            }
+        });
+        //had some concurrecny problem, so always update
+        mandrill_client.templates.update({"name": template_name, "from_email": options.from_email, "from_name": options.from_name, "subject": options.subject, "code": code, "text": options.text, "publish": options.publish, "labels": options.labels}, function(result) {
+           grunt.log.writeln(result);
+           console.log(result);
+        }, function(e) {
+           // Mandrill returns the error as an object with name and message keys
+           grunt.log.writeln('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+           console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
         });
       });
     }
